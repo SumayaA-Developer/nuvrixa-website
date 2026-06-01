919f7ef509067eb7d2144a6d3ed2ddbb812b0098
SumayaA-Developer <sumayaarumugam@gmail.com>
2026-06-01 17:41:25 +0200

    Add Supabase client

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://zmverfkgmazajftkweew.supabase.co',
+  'sb_publishable_H_LmcrsIpCT-gazNEKPUvA_x4_Urg-v'
+);
